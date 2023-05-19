d701ec2f2fae00ebe66bdf506965cf99e39b2c86
bersnakx <87599231+polonkoevv@users.noreply.github.com>
2023-05-19 15:42:45 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -3,9 +3,19 @@
 #include "pow.h"
 using namespace std;
 
-int main(){
+#include <iostream>
+#include <cstdlib>
+
+int main() {
+    const char* a = getenv("PARAMETER_A");
+    const char* b = getenv("PARAMETER_B");
+
     float x = 4;
     int y = 2;
     
     cout << pow(x,y) << endl;
+
+    return 0;
 }
+
+
